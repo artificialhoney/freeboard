3881620f67976336afd57db40db1c00ca5a3ec9f
Sebastian Krüger <sk@honeymachine.io>
2024-12-10 09:58:18 +0100

    feat: player script

--- player.py
+++ player.py
@@ -3,21 +3,23 @@ import sys
 import vlc
 from dotenv import dotenv_values
 
+
 def get_device_type():
     try:
-        with open('/proc/device-tree/model') as file:
+        with open("/proc/device-tree/model") as file:
             content = file.read()
 
-            if 'Raspberry Pi 4' in content:
-                return 'pi4'
-            elif 'Raspberry Pi 3' in content:
-                return 'pi3'
-            elif 'Raspberry Pi 2' in content:
-                return 'pi2'
+            if "Raspberry Pi 4" in content:
+                return "pi4"
+            elif "Raspberry Pi 3" in content:
+                return "pi3"
+            elif "Raspberry Pi 2" in content:
+                return "pi2"
             else:
-                return 'pi1'
+                return "pi1"
     except FileNotFoundError:
-        return 'x86'
+        return "x86"
+
 
 class Player:
     def __init__(self, env):
@@ -25,33 +27,33 @@ class Player:
         options = self.__get_options()
         self.instance = vlc.Instance(options)
         self.player = self.instance.media_player_new()
-        self.player.audio_output_set('alsa')
+        self.player.audio_output_set("alsa")
 
     def get_alsa_audio_device(self):
-        if self.settings['audio_output'] == 'local':
-            return 'plughw:CARD=Headphones'
+        if self.settings["audio_output"] == "local":
+            return "plughw:CARD=Headphones"
         else:
-            if get_device_type() == 'pi4':
-                return 'default:CARD=vc4hdmi0'
-            elif get_device_type() in ['pi1', 'pi2', 'pi3']:
-                return 'default:CARD=vc4hdmi'
+            if get_device_type() == "pi4":
+                return "default:CARD=vc4hdmi0"
+            elif get_device_type() in ["pi1", "pi2", "pi3"]:
+                return "default:CARD=vc4hdmi"
             else:
-                return 'default:CARD=HID'
+                return "default:CARD=HID"
 
     def __get_options(self):
         return [
-            f'--alsa-audio-device={self.get_alsa_audio_device()}',
+            f"--alsa-audio-device={self.get_alsa_audio_device()}",
         ]
 
     def play(self):
-        self.player.set_mrl(self.settings['url'])
-        self.player.audio_output_device_set(
-            'alsa', self.get_alsa_audio_device())
+        self.player.set_mrl(self.settings["url"])
+        self.player.audio_output_device_set("alsa", self.get_alsa_audio_device())
         self.player.play()
 
 
 def main():
     Player(sys.argv[1]).play()
+
 
 if __name__ == "__main__":
     main()
