75b9861e8e1a61ea883c274f8ff59574d2ef3434
Sebastian Krüger <sk@honeymachine.io>
2024-12-10 10:37:43 +0100

    feat: player script

--- player.sh
+++ player.sh
@@ -7,4 +7,21 @@ until curl --output /dev/null --silent --head --fail http://localhost:8088; do
     sleep 5
 done
 
-.venv/bin/python player.py .env.player
+. $SCRIPT_DIR/.env.player
+
+flags=(
+   --kiosk
+   --touch-events=enabled
+   --disable-pinch
+   --noerrdialogs
+   --disable-session-crashed-bubble
+   --simulate-outdated-no-au='Tue, 31 Dec 2099 23:59:59 GMT'
+   --disable-component-update
+   --overscroll-history-navigation=0
+   --disable-features=TranslateUI
+   --autoplay-policy=no-user-gesture-required
+)
+
+# Standard behavior - runs chromium
+chromium-browser "${flags[@]}" --app=$FREEBOARD_PLAYER_URL
+exit;
