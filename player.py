6f4628c9703491b3192753d7bf0948ff9224d373
Sebastian Krüger <sk@honeymachine.io>
2024-12-10 06:58:33 +0100

    feat: service

---
+++ player.py
@@ -0,0 +1,60 @@
+from __future__ import unicode_literals
+
+import vlc
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env.
+
+def get_device_type():
+    try:
+        with open('/proc/device-tree/model') as file:
+            content = file.read()
+
+            if 'Raspberry Pi 4' in content:
+                return 'pi4'
+            elif 'Raspberry Pi 3' in content:
+                return 'pi3'
+            elif 'Raspberry Pi 2' in content:
+                return 'pi2'
+            else:
+                return 'pi1'
+    except FileNotFoundError:
+        return 'x86'
+
+class Player:
+    def __init__(self):
+        options = self.__get_options()
+        self.instance = vlc.Instance(options)
+        self.player = self.instance.media_player_new()
+
+        self.player.audio_output_set('alsa')
+
+    def get_alsa_audio_device(self):
+        if self.settings['audio_output'] == 'local':
+            return 'plughw:CARD=Headphones'
+        else:
+            if get_device_type() == 'pi4':
+                return 'default:CARD=vc4hdmi0'
+            elif get_device_type() in ['pi1', 'pi2', 'pi3']:
+                return 'default:CARD=vc4hdmi'
+            else:
+                return 'default:CARD=HID'
+
+    def __get_options(self):
+        return [
+            f'--alsa-audio-device={self.get_alsa_audio_device()}',
+        ]
+
+    def play(self):
+        self.settings = load_dotenv()
+        self.player.set_mrl(self.settings.url)
+        self.player.audio_output_device_set(
+            'alsa', self.get_alsa_audio_device())
+        self.player.play()
+
+
+def main():
+    Player().play()
+
+if __name__ == "__main__":
+    main()
