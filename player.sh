39b08d4911860eb92f3e643b4834df57c8b41362
Sebastian Krüger <sk@honeymachine.io>
2024-12-10 12:26:19 +0100

    feat: player script

--- player.sh
+++ player.sh
@@ -10,17 +10,20 @@ until curl --output /dev/null --silent --head --fail http://localhost:8088; do
 done
 
 flags=(
-   --window-size=1920,1080
    --kiosk
-   --disable-infobars
-   --noerrdialogs
-   --disable-crash-report
-   --start-fullscreen
-   --start-maximized
+   --window-size=1920,1080
    --window-position=0,0
-   --ignore-certificate-errors
+   --touch-events=enabled
+   --disable-pinch
+   --noerrdialogs
+   --disable-session-crashed-bubble
+   --simulate-outdated-no-au='Tue, 31 Dec 2099 23:59:59 GMT'
+   --disable-component-update
+   --overscroll-history-navigation=0
+   --disable-features=TranslateUI
+   --autoplay-policy=no-user-gesture-required
 )
 
 export DISPLAY=:0.0
 
-sudo -u pi startx /usr/bin/chromium-browser "${flags[@]}" --app=$FREEBOARD_PLAYER_URL --
+startx /usr/bin/chromium-browser "${flags[@]}" --app=$FREEBOARD_PLAYER_URL --
