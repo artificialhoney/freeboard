a6f8d156265fec36de86cec63b5dd73d7cb7a4cb
Sebastian Krüger <sk@honeymachine.io>
2024-12-24 16:49:42 +0100

    fix(ui): css

--- packages/ui/src/components/Header.vue
+++ packages/ui/src/components/Header.vue
@@ -5,6 +5,7 @@ import DashboardControl from "./DashboardControl.vue";
 import FreeboardControl from "./FreeboardControl.vue";
 import ToggleHeaderButton from "./ToggleHeaderButton.vue";
 import ColumnToolbar from "./ColumnToolbar.vue";
+import { RouterLink } from "vue-router";
 
 const freeboardStore = useFreeboardStore();
 const { allowEdit, isEditing, dashboard } = storeToRefs(freeboardStore);
@@ -15,10 +16,12 @@ const { allowEdit, isEditing, dashboard } = storeToRefs(freeboardStore);
     <Transition name="slide-fade">
       <div class="header__admin-bar" v-if="isEditing">
         <div class="header__admin-bar__admin-menu">
-          <h1 class="header__admin-bar__admin-menu__board-logo">
-            <i class="ra ra-feather-wing ra-2x"></i>
-            {{ $t("header.title") }}
-          </h1>
+          <RouterLink to="/"  class="header__admin-bar__admin-menu__board-logo">
+            <h1>
+              <i class="ra ra-feather-wing ra-2x"></i>
+              {{ $t("header.title") }}
+            </h1>
+          </RouterLink>
           <div class="header__admin-bar__admin-menu__board-tools">
             <div
               class="header__admin-bar__admin-menu__board-tools__board-actions"
